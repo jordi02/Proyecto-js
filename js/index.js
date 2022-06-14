111de13b0b4b41ccf57dec878d0efe48149bf3ec
jordi <jordigimbernat02@gmail.com>
2022-06-14 17:22:22 -0300

    resuelto

--- js/index.js
+++ js/index.js
@@ -6,34 +6,16 @@ const precio = document.createElement("div")
 precio.innerHTML = '<label>INGRESE PRECIO DE VENTA EN UN PAGO</label> <input type=number id=DineroIngresado>'
 document.body.appendChild(precio)
 
-const cuotas = document.createElement("div")
-cuotas.innerHTML = `<label>SELECIONE LOS MESES A PAGAR</label> <select id=select>
-<option disabled="" value="">Selecciona un valor</option>
-<option value="2">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-<option value="5">5</option>
-<option value="6">6</option>
-<option value="7">7</option>
-<option value="8">8</option>
-<option value="9">9</option>
-<option value="10">10</option>
-<option value="11">11</option>
-<option value="12">12</option>
-<option value="13">13</option>
-<option value="14">14</option>
-<option value="15">15</option>
-<option value="16">16</option>
-<option value="17">17</option>
-<option value="18">18</option>
-<option value="19">19</option>
-<option value="20">20</option>
-<option value="21">21</option>
-<option value="22">22</option>
-<option value="23">23</option>
-<option value="24">24</option>
-</select> `
-document.body.appendChild(cuotas)
+let cuotas2 = document.createElement("div")
+let opciones = "<label>Seleccione las cuotas a pagar </label><select id=select>"
+
+for (let i = 1; i <= 24; i++) {
+
+    opciones += `<option value=${i}>${i}</option>`
+}
+opciones += "</select>"
+cuotas2.innerHTML = opciones
+document.body.appendChild(cuotas2)
 
 const porcentaje = document.createElement("div")
 porcentaje.innerHTML = '<label>Ingrese el porcentaje de interes de sus cuotas</label> <input type=number id=PorcentajeIngresado>'
@@ -51,7 +33,7 @@ enviar.addEventListener("click", () => {
         text: "Operacion exitosa",
         duration: 2000,
         gravity: `bottom`,
-        position: `rigth`,
+        position: `right`,
     }).showToast()
 
 })
