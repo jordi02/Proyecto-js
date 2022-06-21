fb9a56f83152214d634b89da72d2fc2dedb1a361
jordi <jordigimbernat02@gmail.com>
2022-06-21 19:25:12 -0300

    pokeapi

--- js/index.js
+++ js/index.js
@@ -77,4 +77,8 @@ enviar.addEventListener("click", () => {
     }).showToast()
 
 })
-
+fetch('https://pokeapi.co/api/v2/')
+    // Exito
+    .then(response => response.json())  // convertir a json
+    .then(json => console.log(json))    //imprimir los datos en la consola
+    .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
